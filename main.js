29672d2fb6278ff34710d320bb06aa9ff84ca15d
amithabh <amithabhshibin@gmail.com>
2024-08-22 11:25:48 +0400

    full-black commit

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 
-
-
 document.addEventListener('DOMContentLoaded', function() {
     const navLinks = document.getElementById('nav-links');
     navLinks.classList.remove('active');
@@ -12,14 +10,12 @@ document.addEventListener('DOMContentLoaded', function() {
         navLInks.classList.toggle('active');
     } );
 
-
-
     $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
     items: 1,
     loop: true,
     autoplay: true,
-    autoplayTimeout: 5000, // Change slide every 5 seconds
+    autoplayTimeout: 5000, 
     autoplayHoverPause: true,
     nav: false,
     dots: true,
@@ -30,72 +26,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-  
-// Change slide every 5 seconds
-
-
-    document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll('img');
-
-    const loadImage = (img) => {
-        const src = img.getAttribute('data-src');
-        if (!src) return;
-        img.src = src;
-    };
-
-    const imgOptions = {
-        threshold: 0,
-        rootMargin: "0px 0px 200px 0px"
-    };
-
-    const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-        entries.forEach(entry => {
-            if (!entry.isIntersecting) {
-                return;
-            } else {
-                loadImage(entry.target);
-                imgObserver.unobserve(entry.target);
-            }
-        });
-    }, imgOptions);
-
-    images.forEach(image => {
-        imgObserver.observe(image);
-    });
-});
-
-// Change slide every 5 seconds
-
-document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll('.gallery-image');
-    const modal = document.getElementById("imageModal");
-    const modalImg = document.getElementById("modalImage");
-    const closeBtn = document.querySelector(".close");
-
-    images.forEach(image => {
-        image.addEventListener('click', function() {
-            modal.style.display = "block";
-            modalImg.src = this.getAttribute('data-full');
-        });
-    });
-
-    closeBtn.addEventListener('click', function() {
-        modal.style.display = "none";
-    });
-
-    // Close the modal if the user clicks outside the image
-    window.addEventListener('click', function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    });
-});
 
 
 
 
 
-// Change slide every 5 seconds
+
+
+
 
 
 
